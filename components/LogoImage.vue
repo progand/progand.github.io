3b997a2110e3f90c900239b711623c5e535c9fe0
progand <lach@ukr.net>
2022-11-03 11:15:42 +0200

    Changed logo

--- components/LogoImage.vue
+++ components/LogoImage.vue
@@ -10,31 +10,31 @@
       style="fill: rgba(130, 55, 209, 0.73)"
       width="49"
       height="49"
-      rx="10"
-      ry="10"
+      rx="15"
+      ry="15"
     ></rect>
     <rect
       style="fill: rgb(209, 209, 121)"
       width="49"
       height="49"
-      rx="10"
-      ry="10"
+      rx="15"
+      ry="15"
       x="50"
     ></rect>
     <rect
       style="fill: rgb(133, 132, 8)"
       width="49"
       height="49"
-      rx="10"
-      ry="10"
+      rx="15"
+      ry="15"
       y="50"
     ></rect>
     <rect
       style="fill: rgb(75, 21, 133)"
       width="49"
       height="49"
-      rx="10"
-      ry="10"
+      rx="15"
+      ry="15"
       x="50"
       y="50"
     ></rect>
