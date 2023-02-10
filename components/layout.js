e6ac68079886dc21464ba173bfa99e4a53d90c48
pkhodakovsky <pavel.khodakovsky1996@gmail.com>
2023-02-10 16:12:54 +0200

    Fixed dark mode

--- components/layout.js
+++ components/layout.js
@@ -25,27 +25,7 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      {/*  <Navbar className="">
-        {home ? (
-          <>
-            <div className="h1">{name}</div>
-          </>
-        ) : (
-          <>
-            <div className="">
-              <Link href="/" className="">
-                {name}
-              </Link>
-            </div>
-          </>
-        )}
-      </Navbar> */}
       <Navbar />
-      {!home && (
-        <div className="">
-          <Link href="/">← Back to home</Link>
-        </div>
-      )}
       <main>{children}</main>
       <Footer />
     </div>
